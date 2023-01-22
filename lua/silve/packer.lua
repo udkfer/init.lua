cdaf036c101150ece85471d1f057b096b5bf6a91
udkfer <udkfer@gmail.com>
2023-01-22 15:34:16 -0300

    adjust

--- lua/silve/packer.lua
+++ lua/silve/packer.lua
@@ -27,7 +27,7 @@ return require('packer').startup(function(use)
 	use('theprimeagen/harpoon')
 	use('mbbill/undotree')
 	use('tpope/vim-fugitive')
-	
+
 	use {
 		'VonHeikemen/lsp-zero.nvim',
 		requires = {
